981d9730f7f659313533971e0b9441400c147f07
Lucas Pintos <lucaspintos909@gmail.com>
2021-03-24 16:34:35 -0300

    [FIX] Cambiando CDN de Bootstrap por una valida

--- includes/header.php
+++ includes/header.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a href="index.php" class="navbar-brand">Crud PHP Mysql</a>
+        <a href="index.php" class="navbar-brand">Crud Tasks PHP - Mysql</a>
         <span class="navbar-text">
             Made by Lucas Pintos
         </span>
