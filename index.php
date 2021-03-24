d6f2daa4546181ce50b8de897d4e57dccde5a2d7
Lucas Pintos <lucaspintos909@gmail.com>
2021-03-24 18:59:07 -0300

    [FEAT] Mensaje de alerta de estado al crear tarea

--- index.php
+++ index.php
@@ -5,12 +5,21 @@
 <?php
   include('./includes/header.php')
 ?>
-
 <div class="container p-4">
-
+    <div>
+        <?php if(isset($_SESSION['message'])){ ?>
+            <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+                <?= $_SESSION['message'] ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <?php  session_unset();} ?>
+    </div>
     <div class="row">
 
         <div class="col-md-4">
+            
             <div class="card card-body">
                 <form action="save_task.php" method="POST">
 
@@ -24,9 +33,10 @@
                         <textarea name="description" class="form-control" rows="2" placeholder="Type here"></textarea>
                     </div>
                     <input class="btn btn-success btn-block" type="submit" name="save_task" value="Save task">
-                
+
                 </form>
             </div>
+            
         </div>
         <div class="col-md-4"></div>
     </div>
