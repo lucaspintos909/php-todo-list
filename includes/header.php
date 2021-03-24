5dbc94a853aabb608b5b44d10b4b3a04f280d3f0
Lucas Pintos <lucaspintos909@gmail.com>
2021-03-24 19:01:00 -0300

    Cambiando contenido del navbar

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
+    <title>Tasks</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
@@ -13,7 +13,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a href="index.php" class="navbar-brand">Crud Tasks PHP - Mysql</a>
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
         <span class="navbar-text">
             Made by Lucas Pintos
         </span>
