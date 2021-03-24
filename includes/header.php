4ef93e19afce7f6162c112213972efb2ca2b6606
Lucas Pintos <lucaspintos909@gmail.com>
2021-03-24 19:43:10 -0300

    [Update] Añadiendo fontawesome por cdn

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tasks</title>
+    <title>Tasks CRUD</title>
+    
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/a5d3bd51fc.js" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
