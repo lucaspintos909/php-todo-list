1e943330f152b79f0284655cd9cc30b174e0ff79
Lucas Pintos <lucaspintos909@gmail.com>
2021-03-24 16:35:36 -0300

    [FIX] Quitando linea que cierra coneccion a la BD

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 
 //phpinfo();
-$connection = mysqli_connect('db', 'root', '', 'php-mysql');
+$conn = mysqli_connect('db', 'root', '', 'php-mysql');
 
 if(isset($connection)){
-    //print("DB connected!!   ");
+    print("DB connected!!   ");
 }
 
-mysqli_close($connection)
+//mysqli_close($connection)
 ?>
