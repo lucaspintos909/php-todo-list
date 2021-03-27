ae8e00241135c307a1b569f1a83cedf33dbcbfc9
Lucas Pintos <lucaspintos909@gmail.com>
2021-03-27 11:17:42 -0300

    [UPDATE] Quitando estilos y javascript

--- index.php
+++ index.php
@@ -129,34 +129,6 @@ include('./src/includes/header.php');
 
 </div>
 
-<script>
-    /*Funcion para mostrar el modal de editar tarea, poniendole los datos a los inputs*/
-function showModal(task_id, task_title, task_description) {
-    $('#editTaskModal').modal('show');
-    $('#modal_id_input').val(task_id);
-    $('#modal_title_input').val(task_title);
-    $('#modal_description_input').val(task_description);
-
-}
-</script>
-
-<style>
-.buttons {
-    display: flex;
-    justify-content: space-between;
-}
-
-.alert-fixed {
-    width: fit-content;
-    margin: auto;
-}
-
-.text-center {
-    text-align: center;
-}
-</style>
-
-
 
 <?php
 include('./src/includes/footer.php')
