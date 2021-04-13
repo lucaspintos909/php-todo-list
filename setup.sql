1ea7cfef1482ed3cbb76ee691f90fe5183716579
Lucas Pintos <lucasmateo.pintos@outlook.com>
2021-04-13 14:36:16 -0300

    [UPDATE] Creando base de datos y cambiando nombre

--- setup.sql
+++ setup.sql
@@ -1,7 +1,10 @@
-CREATE TABLE `php-mysql`.`task` (
+-- Creando la base de datos
+CREATE DATABASE IF NOT EXISTS `php-mysql`;
+
+-- Creando tablas
+CREATE TABLE IF NOT EXISTS `php-mysql`.`task` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `description` TEXT NOT NULL,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`));
-  
