4bb7541ba505e42f4da4eb1847d3367a80106455
Lucas Pintos <lucasmateo.pintos@outlook.com>
2021-04-13 20:37:39 -0300

    [UPDATE] Añadiendo email de usuario a tabla tareas

--- setup.sql
+++ setup.sql
@@ -1,19 +1,21 @@
--- Creando la base de datos
+-- Creating the database
 CREATE DATABASE IF NOT EXISTS task_system;
 
--- Creando tablas
+-- Creating tables
 CREATE TABLE IF NOT EXISTS task_system.task (
   id INT(11) AUTO_INCREMENT,
+  user_email INT(11) NOT NULL,
   title VARCHAR(50) NOT NULL,
   description TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_email) REFERENCES users(email)
 );
 
 CREATE TABLE IF NOT EXISTS task_system.users (
 	id INT(11) AUTO_INCREMENT,
-    user_name VARCHAR(120) NOT NULL,
-    email VARCHAR(120) NOT NULL,
-    password VARCHAR(256) NOT NULL,
-    primary key(id)
+  user_name VARCHAR(120) NOT NULL,
+  email VARCHAR(120) NOT NULL UNIQUE,
+  password VARCHAR(256) NOT NULL,
+  PRIMARY KEY(id)
 );
