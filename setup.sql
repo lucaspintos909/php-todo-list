ac6ca311254598bdfb447943aacec80c682cbec4
Lucas Pintos <lucasmateo.pintos@outlook.com>
2021-04-13 20:22:34 -0300

    [FEAT] Tabla de usuarios

--- setup.sql
+++ setup.sql
@@ -1,10 +1,19 @@
 -- Creando la base de datos
-CREATE DATABASE IF NOT EXISTS `php-mysql`;
+CREATE DATABASE IF NOT EXISTS task_system;
 
 -- Creando tablas
-CREATE TABLE IF NOT EXISTS `php-mysql`.`task` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NOT NULL,
-  `description` TEXT NOT NULL,
-  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS task_system.task (
+  id INT(11) AUTO_INCREMENT,
+  title VARCHAR(50) NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS task_system.users (
+	id INT(11) AUTO_INCREMENT,
+    user_name VARCHAR(120) NOT NULL,
+    email VARCHAR(120) NOT NULL,
+    password VARCHAR(256) NOT NULL,
+    primary key(id)
+);
