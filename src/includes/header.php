aa01667b8b2b34187f03d1ebd4a92cc4cb865fde
Lucas Pintos <lucasmateo.pintos@outlook.com>
2021-04-13 20:01:04 -0300

    [UPDATE] Añadi icono en la pestaña

--- src/includes/header.php
+++ src/includes/header.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/favicon.svg" />
     <title>Tasks CRUD</title>
-
+    
     <!--  Mis estilos  -->
     <link rel="stylesheet" href="./assets/styles.css">
 
