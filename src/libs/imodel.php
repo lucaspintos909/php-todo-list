8fecf085f953fac7f562d32e76b2b9863bbf3b89
Lucas Pintos <lucasmateo.pintos@outlook.com>
2021-04-14 19:59:23 -0300

    [FEAT] Interfaz para aplicar en modelos

---
+++ src/libs/imodel.php
@@ -0,0 +1,13 @@
+<?php
+
+interface IModel{
+
+    public function save();
+    public function getAll();
+    public function get($id);
+    public function delete($id);
+    public function update();
+    public function from($array);
+
+
+}
