677da16ad32ff294f7c48c7d2f31888316d50689
Lucas Pintos <lucasmateo.pintos@outlook.com>
2021-04-14 14:19:01 -0300

    [FEAT] Defino variables tanto de DB como de la URL

---
+++ src/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+# Url de la pagina
+define('URL', 'http://localhost:8080/');
+
+# Base de datos
+define('HOST', 'db');
+define('DB', 'task_db');
+define('USER', 'root');
+define('PASSWORD', 'root');
+define('CHARSET', 'utf8mb4');
+
+
+
