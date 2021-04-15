5c32f5799a4334bf5536c9e776783c78bde191ef
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-15 11:20:11 -0300

    [UPDATE] Scripts bootstrap para las notificaciones

--- src/views/login/index.php
+++ src/views/login/index.php
@@ -10,7 +10,7 @@
 
 </head>
 <body>
-
+    
     <div class="container mt-5">
         <div class="col-md-4 offset-md-4">
             <div class="card">
@@ -35,7 +35,6 @@
                         <input name="password" id="password" class="form-control" type="password" />
                         <a href="#" class="mt-2">¿Olvidaste la contraseña?<a>
                     </div>
-
                     <div class="form-group mt-2">
                         <button class="btn btn-primary btn-block">Ingresar</button>
                     </div>
@@ -50,6 +49,8 @@
             </div>
         </div>
     </div>
-        
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
