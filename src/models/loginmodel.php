ab669b7fa10dadc9a8fe9f90979f95e4aeb00e8c
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-16 13:26:04 -0300

    [FEAT] Login

--- src/models/loginmodel.php
+++ src/models/loginmodel.php
@@ -1,10 +1,43 @@
 <?php
   
-    class LoginModel extends Model{
+require_once 'models/loginmodel.php';
+class LoginModel extends Model{
 
-        function __construct(){
-            parent::__construct();
+    function __construct(){
+        parent::__construct();
+        
+    }
+
+    function login($email, $password){
+        try {
             
-        }
+            $query = $this->prepare('SELECT * FROM task_db.users WHERE email = :email');
+            
+            $query->execute([
+                'email' => $email
+            ]);
+            
+            # Si rowCount() devuelve 0 usuarios es que no existe
+            if($query->rowCount() == 1){
+                $item = $query->fetch(PDO::FETCH_ASSOC);
 
-    } 
+                $user = new UserModel();
+
+                # Ingreso el usuario de la bd en el usuario del modelo
+                $user->from($item);
+                
+                # Verifica que las dos contraseñas sean iguales, y si no son iguales retorna null
+                if(password_verify($password, $user->getPassword())) {
+                    return $user; 
+                }else{
+                    return NULL;
+                }
+
+            }                
+
+        } catch (PDOException $ex) {
+            error_log("LloginModel::login->exception " . $ex);
+            return NULL;
+        }
+    }
+} 
