ec192e1d027f2d3076c0688c5cbf51fae936103c
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-15 21:43:35 -0300

    [UPDATE] Modificando  el diseño del login

--- src/views/login/index.php
+++ src/views/login/index.php
@@ -12,7 +12,7 @@
 <body>
     
     <div class="container mt-5">
-        <div class="col-md-4 offset-md-4">
+        <div class="col-md-6 m-auto">
             <div class="card">
                 <div class="card-header text-center">
                     <h4>Iniciar Sesión</h4>
@@ -20,30 +20,30 @@
 
                 <div class="card-body">
                     <form>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input name="email" id="email" class="form-control" type="email" />
-                    </div>
-
-                    <div class="form-group">
-                        <div class="row">
-                        <div class="col-3">
-                            <label for="password">Contraseña</label>
-                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input name="email" id="email" class="form-control" type="email" />
                         </div>
 
-                        <input name="password" id="password" class="form-control" type="password" />
-                        <a href="#" class="mt-2">¿Olvidaste la contraseña?<a>
-                    </div>
-                    <div class="form-group mt-2">
-                        <button class="btn btn-primary btn-block">Ingresar</button>
-                    </div>
+                        <div class="form-group">
+                            <div class="row">
+                            <div class="col-3">
+                                <label for="password">Contraseña</label>
+                            </div>
+                            </div>
+
+                            <input name="password" id="password" class="form-control" type="password" />
+                            <a href="#" class="mt-2">¿Olvidaste la contraseña?<a>
+                        </div>
+                        <div class="form-group mt-2">
+                            <button class="btn btn-primary btn-block">Ingresar</button>
+                        </div>
                     </form>
 
                     <p class="text-center">O</p>
 
                     <div class="form-group">
-                    <a href="#" class="btn btn-success btn-block">Registrarse</a>
+                    <a href="<?$URL?>/signup" class="btn btn-success btn-block">Registrarse</a>
                     </div>
                 </div>
             </div>
