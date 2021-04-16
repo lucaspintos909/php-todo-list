523704f7bbfeb1e3c8e7f32d759964615daa5c47
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-15 21:43:28 -0300

    [UPDATE] Modificando  el diseño del registro

--- src/views/login/signup.php
+++ src/views/login/signup.php
@@ -5,8 +5,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 </head>
 <body>
-    <h1>Registro</h1>
+    
+    <div class="container mt-5">
+        <div class="col-md-12 offset-md-1asd">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h4>Registrarse</h4>
+                </div>
+
+                <div class="card-body">
+                    <form class="row g-3"> 
+
+                        <div class="form-group col-md-6">
+                            <label for="username">Nombre de ususario</label>
+                            <input name="username" id="username" class="form-control" type="text" placeholder="Ej. Pepe"/>
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="email">Email</label>
+                            <input name="email" id="email" class="form-control" type="email" placeholder="nombre@ejemplo.com"/>
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="password">Contraseña</label>
+                            <input name="password" id="password" class="form-control" type="password" placeholder="Ingrese su contraseña"/>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="confirm_password">Confirmar contraseña</label>
+                            <input name="confirm_password" id="confirm_password" class="form-control" type="password" placeholder="Repita su contraseña" />
+                        </div>
+                        <div class="form-group mt-2 m-auto col-md-6">
+                            <a href="#" class="btn btn-success btn-block">Registrarse</a>
+                        </div>
+                    </form>
+
+                    <p class="text-center mt-2">O</p>
+
+                    <div class="form-group m-auto col-md-5">
+                        <a href="<?$URL?>/login" class="btn btn-primary btn-block">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
 </html>
