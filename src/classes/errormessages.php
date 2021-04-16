084f50f015a14bc589e62d6d706076987e561a4d
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-16 00:58:37 -0300

    [FEAT] Añadiendo mensajes de error para signup

--- src/classes/errormessages.php
+++ src/classes/errormessages.php
@@ -7,6 +7,13 @@ class ErrorMessages{
     const ERROR_TASK_DELETE = 'PcAXeF8q';
     const ERROR_TASK_UPDATE = 'd8mXEbmZ';
 
+    const ERROR_USER_CREATED = '29c7TowW';
+    const ERROR_USER_EMPTY = 'aqX2kc7E';
+    const ERROR_USER_PASSWORD = 'rp4b463Q';
+    const ERROR_USER_EMAIL = 'R8qPVDZP';
+    const ERROR_USER_EXISTS = 'Wbht9ACn';
+
+
     private $error_list = [];
 
     public function __construct(){
@@ -14,7 +21,12 @@ class ErrorMessages{
         $this->error_list= [
             ErrorMessages::ERROR_TASK_CREATE => 'Hubo un error al crear la tarea, intente nuevamente.',
             ErrorMessages::ERROR_TASK_DELETE => 'Hubo un error al eliminar la tarea, intente nuevamente.',
-            ErrorMessages::ERROR_TASK_UPDATE => 'Hubo un error al modificar la tarea, intente nuevamente.'
+            ErrorMessages::ERROR_TASK_UPDATE => 'Hubo un error al modificar la tarea, intente nuevamente.',
+            ErrorMessages::ERROR_USER_EMPTY => 'Has dejado campos incompletos.',
+            ErrorMessages::ERROR_USER_PASSWORD => 'Las contraseñas no coinciden.',
+            ErrorMessages::ERROR_USER_EMAIL => 'El mail ingresado es incorrecto.',
+            ErrorMessages::ERROR_USER_EXISTS => 'El nombre de usuario ingresado ya existe, ingrese otro.',
+            ErrorMessages::ERROR_USER_CREATED => 'Hubo un error al intentar crear el usuario, intente nuevamente.',
         ];
 
     }
