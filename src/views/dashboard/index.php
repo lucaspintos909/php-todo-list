97f426c2981b24be68033c85293dd4f50a100555
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-16 15:56:46 -0300

    [UPDATE] Quitando botones de login y signup

--- src/views/dashboard/index.php
+++ src/views/dashboard/index.php
@@ -49,11 +49,14 @@
     <div class="collapse navbar-collapse " id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
+                <a class="nav-link" href="<?php constant('URL')?>logout">Cerrar Sesion</a>
+            </li>
+            <!-- <li class="nav-item">
                 <a class="nav-link" href="<?php constant('URL')?>signup">Registro</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?php constant('URL')?>login"">Login</a>
-            </li>
+            </li> -->
         </ul>
         
     </div>
