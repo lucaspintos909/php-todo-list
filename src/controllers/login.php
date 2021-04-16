f3fb96638792d0e369429540111af4a43c9c7a4b
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-16 13:24:42 -0300

    [FEAT] Funcion authenticate

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'models/loginmodel.php';
 class Login extends SessionController{
 
     function __construct(){
@@ -9,5 +10,36 @@ class Login extends SessionController{
 
     function render(){
         $this->view->render('login/index');
+    }
+
+    function authenticate(){
+        if($this->existPOST(['email', 'password'])){
+            $email = $this->getPOST('email');
+            $password = $this->getPOST('password');
+
+            # Verifica si hay campos vacios
+            if($this->emptyVariables([$email, $password])){
+                
+                $this->redirect('login',['error' => ErrorMessages::ERROR_USER_EMPTY]);
+            
+            # Verifica si el mail es valido
+            }elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $this->redirect('login',['error' => ErrorMessages::ERROR_USER_EMAIL]);
+            }
+            #error_log("&&&&&&&&&&&&&&&&&&&&&&&&&&& " . var_dump($this));
+            
+            #$model = new LoginModel();
+            $user = $this->model->login($email,$password);
+            
+            if($user != NULL){
+                $this->initialize($user);
+            }else{
+                $this->redirect('login',['error' => ErrorMessages::ERROR_USER_INCORRECT]);
+            }
+
+        }else {
+            $this->redirect('login',['error' => ErrorMessages::ERROR_USER_CREATED]);
+        }
+
     }
 }
