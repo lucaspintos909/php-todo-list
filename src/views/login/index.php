237d34f5f31cd11a3b83618ce89b3ad285829c59
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-16 10:57:14 -0300

    [UPDATE] Iconos en login signup

--- src/views/login/index.php
+++ src/views/login/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-
+    <link rel="shortcut icon" href="assets/icons/login-blue.svg" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
 </head>
