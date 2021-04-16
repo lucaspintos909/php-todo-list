824e4bebe8c2e29740859a8beb784bf05481cdc0
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-16 18:42:24 -0300

    [UPDATE] Cambiando nombre de funciones

--- src/libs/imodel.php
+++ src/libs/imodel.php
@@ -2,11 +2,11 @@
 
 interface IModel{
 
-    public function saveUser();
-    public function getAllUsers();
-    public function getUser($id);
-    public function deleteUser($id);
-    public function updateUser();
+    public function save();
+    public function getAll();
+    public function get($id);
+    public function delete($id);
+    public function update();
     public function from($array);
 
 
