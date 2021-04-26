baa086053242d7e89bae458de74fc8c306baae78
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-26 16:35:43 -0300

    [FEAT] Admin controller, model y view

---
+++ src/models/adminmodel.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class AdminModel extends Model implements IModel
+{
+
+    public function save()
+    {
+        // TODO: Implement save() method.
+    }
+
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function update()
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function from($array)
+    {
+        // TODO: Implement from() method.
+    }
+}
