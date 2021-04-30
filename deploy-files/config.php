a500ad8ee5b0cf70b529708c2d15d85eb4042aef
Lucas Pintos <lucaspintos909@gmail.com>
2021-04-30 12:11:46 -0300

    [FEAT] Configuracion para el deploy

---
+++ deploy-files/config.php
@@ -0,0 +1,15 @@
+<?php
+
+# Url de la pagina
+const URL = "http://example.com/";
+
+# Base de datos
+const HOST = 'db';
+const DB = 'task_db';
+const USER = 'admin';
+const PASSWORD = 'admin';
+const CHARSET = 'utf8mb4';
+
+
+
+
