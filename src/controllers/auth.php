0acd517bae0fbec874f6a1e97c12084e25ea2ba1
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-04 11:27:31 -0300

    refactor: Quitando metodos innecesarios

--- src/controllers/auth.php
+++ src/controllers/auth.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once 'models/authmodel.php';
 class Auth extends SessionController{
 
     function __construct(){
@@ -11,32 +10,4 @@ class Auth extends SessionController{
         $this->view->render('auth/index');
     }
 
-    function authenticate(){
-        if($this->existPOST(['email', 'password'])){
-            $email = $this->getPOST('email');
-            $password = $this->getPOST('password');
-
-            # Verifica si hay campos vacios
-            if($this->emptyVariables([$email, $password])){
-                
-                $this->redirect('auth',['error' => ErrorMessages::ERROR_USER_EMPTY]);
-            
-            # Verifica si el mail es valido
-            }elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $this->redirect('auth',['error' => ErrorMessages::ERROR_USER_EMAIL]);
-            }
-            
-            $user = $this->model->login($email, $password);
-
-            if($user != NULL){
-                $this->initialize($user);
-            }else{
-                $this->redirect('auth',['error' => ErrorMessages::ERROR_USER_INCORRECT]);
-            }
-
-        }else {
-            $this->redirect('auth',['error' => ErrorMessages::ERROR_USER_CREATED]);
-        }
-
-    }
 }
