2f4ddcbcf9aeca482ba92a7362b16e91ecd0c947
Lucas Pintos <lucaspintos909@gmail.com>
2021-05-03 21:06:50 -0300

    feat: Tabla de usuarios para seccion admin

--- src/views/admin/index.php
+++ src/views/admin/index.php
@@ -1,23 +1,34 @@
+<title>Admin Page</title>
 <?php
 include_once 'views/includes/header.php';
 ?>
-
 <div class="container">
 
-    <h1 class="">Admin Page</h1>
+    <h1 class="text-center mt-4">Admin Page</h1>
+    <div class="col-md-8 m-auto pt-4">
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nombre de usuario</th>
+                <th>Email</th>
+                <th>Rol</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($this->data['data'] as $user){ ?>
+                <tr>
+                    <td><?php echo $user->getId(); ?></td>
+                    <td><?php echo $user->getUsername(); ?></td>
+                    <td><?php echo $user->getEmail(); ?></td>
+                    <td><?php echo $user->getRole(); ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
 
-    <?php
-        foreach ($this->data as $user){
-            echo $user->getId();
-            echo "<br>";
-            echo $user->getUsername();
-            echo "<br>";
-            echo $user->getEmail();
-            echo "<br>";
-            echo $user->getRole();
-            echo "<br>";
-        }
-    ?>
 
 
 </div>
